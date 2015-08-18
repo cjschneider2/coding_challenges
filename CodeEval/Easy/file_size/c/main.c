f2588bc8b13b8f6551c2536a56b05d824db1dbb2
cjschneider2 <cjschneider2@gmail.com>
2015-08-18 05:30:14 +0200

    feat(file_size): completed challenge

--- CodeEval/Easy/file_size/c/main.c
+++ CodeEval/Easy/file_size/c/main.c
@@ -2,14 +2,31 @@
 
 int main (int argc, const char * argv[])
 {
-
-    FILE *file = fopen(argv[1], "r");
-    char line[1024];
-
-    while (fgets(line, 1024, file))
-    {
-        printf("%s\n", line);
+/* Program Setup */
+    /* Check Inputs */
+    if (argc > 2) {
+        printf ("argc: %d\n", argc);
+        printf ("argv[1]: %s", argv[1]);
     }
-
+    /* Variables */
+    FILE
+        *file = fopen(argv[1], "r");
+    int
+        c = 0,
+        bit_cnt = 0;
+/* Program Logic */
+    /* Check for open file */
+    if (!file)
+        return 1;
+    do
+    {
+        c = fgetc (file);
+        if (c != EOF)
+            bit_cnt++;
+    }
+    while ( c != EOF );
+    /* Print the output */
+    printf ("%d\n", bit_cnt);
+/* Cleanup */
     return 0;
 }
